15bda3c6ba5d89be767d7d4635c5b7e620235689
ChammounC <chingchakhap11@gmail.com>
2023-05-21 17:06:38 +0530

    updated MainLayout

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Box,ListItemText, Toolbar, Typography } from "@mui/material";
+import { Box, ListItemText, Toolbar, Typography } from "@mui/material";
 import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import Sidebar from '../common/Sidebar';
+import Sidebar from "../common/Sidebar";
 
-import sizeConfigs from '../../configs/sizeConfigs';
-import colorConfigs from '../../configs/colorConfigs';
-import { Outlet } from "react-router-dom";
+import sizeConfigs from "../../configs/sizeConfigs";
+import colorConfigs from "../../configs/colorConfigs";
+import { Outlet, useLocation } from "react-router-dom";
+import BottomNav from "../common/BottomNav";
 
 const MainLayout = () => {
-
-
+  const location = useLocation();
   return (
     <Box sx={{ display: "flex" }}>
       <Box
@@ -22,62 +22,63 @@ const MainLayout = () => {
       >
         <Sidebar />
       </Box>
-      <Box
-        component="main"
-        sx={{
-          justifyContent: "space-between",
-          flexGrow: 1,
-          p: "56px",
-          width: `calc(100% - ${sizeConfigs.sidebar.width})`,
-          minHeight: "100vh",
-          backgroundColor: colorConfigs.mainBg,
-        }}
-      >
-        <Toolbar
-        style={{
-          padding:'0px',
-          minHeight:'0px',
-        }}
+        <Box
+          component="main"
           sx={{
-            display: "flex",
-            flexDirection: "row",
             justifyContent: "space-between",
-            marginBottom:'48px'
+            flexGrow: 1,
+            p: "56px",
+            width: `calc(100% - ${sizeConfigs.sidebar.width})`,
+            minHeight: "100vh",
+            backgroundColor: colorConfigs.mainBg,
           }}
         >
-          <Typography
+          <Toolbar
+            style={{
+              padding: "0px",
+              minHeight: "0px",
+            }}
             sx={{
-              fontWeight: 700,
-              fontSize: "28px",
-              lineHeight: "36px",
-              letterSpacing: "0.02em",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: "48px",
             }}
           >
-            Test_Study
-          </Typography>
-          <div style={{ display: "flex", alignItems: "center",gap:'8px' }}>
-            <AccountCircleIcon sx={{ width: "30px", height: "30px" }} />
-            <ListItemText
-              // disableTypography
-              primary={
-                <Typography
-                  sx={{
-                    fontWeight: 400,
-                    fontSize: "16px",
-                    lineHeight: "21px",
-                    letterSpacing: "0.015em",
-                    flex: "5",
-                  }}
-                >
-                  Tipchan Songtheng
-                </Typography>
-              }
-            />
-            <ExpandMoreOutlinedIcon />
-          </div>
-        </Toolbar>
-        <Outlet />
-      </Box>
+            <Typography
+              sx={{
+                fontWeight: 700,
+                fontSize: "28px",
+                lineHeight: "36px",
+                letterSpacing: "0.02em",
+              }}
+            >
+              Test_Study
+            </Typography>
+            <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+              <AccountCircleIcon sx={{ width: "30px", height: "30px" }} />
+              <ListItemText
+                // disableTypography
+                primary={
+                  <Typography
+                    sx={{
+                      fontWeight: 400,
+                      fontSize: "16px",
+                      lineHeight: "21px",
+                      letterSpacing: "0.015em",
+                      flex: "5",
+                    }}
+                  >
+                    Ed Sheeran
+                  </Typography>
+                }
+              />
+              <ExpandMoreOutlinedIcon />
+            </div>
+          </Toolbar>
+          <Outlet />
+        {location.pathname.includes('/montages')&&<BottomNav />}
+        </Box>
     </Box>
   );
 };
